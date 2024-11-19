05ef8373cd8f0cd8447a23f1eb050c6b8b959643
Deniz Unal <salihdenizunal@gmail.com>
2024-11-19 22:25:59 +0300

    Fix food model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,8 @@
 from models.user_model.user_model import User
+from models.food_model.food_model import Food
 
 __models__ = [
     # Main models
     User,
+    Food,
 ]
--- app/models/food_model/food_model.py
+++ app/models/food_model/food_model.py
@@ -3,13 +3,13 @@ from pydantic import Field, BaseModel
 from fastapi import Form
 
 class Food(Document):
-    food_type: str = Field(..., example="Fruit")
+    food_type: str = Field(..., example="Pizza")
     count: int = Field(0, example=10)
     vendor_name: str = Field(..., example="John Doe")
-   
-
+    # TODO: link the user instead of the vendor name
+    # vendor: User = Field(..., example=User)
 
 class CreateFood(BaseModel):
-    food_type: str = Form(..., example="Fruit")
+    food_type: str = Form(..., example="Pizza")
     count: int = Form(..., example=10)
     vendor_name: str = Form(..., example="John Doe")
